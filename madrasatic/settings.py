dce0aa13e0b4ca996931c1b2ac888411d2f9c56b
assia-mad <as.madaniyousfi@esi-sba.dz>
2022-04-08 17:44:28 +0200

    adding parsers to updateprofile view

--- madrasatic/settings.py
+++ madrasatic/settings.py
@@ -196,10 +196,13 @@ REST_FRAMEWORK = {
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
-
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://frontend:3000',
     'http://0.0.0.0:3000',
 )
+
+
+
+
